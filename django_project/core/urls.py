e85dadb72443a841401b0c8995e391baf8ef561d
Dražen Odobašić <dodobas@candela-it.com>
2014-10-22 22:44:02 +0200

    Expose static files and uploded media if DEBUG is active
    Closes #6

--- django_project/core/urls.py
+++ django_project/core/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 from django.contrib import admin
-admin.autodiscover()
 
 
 urlpatterns = patterns(
@@ -17,3 +17,15 @@ urlpatterns = patterns(
     # url(r'', include('application.urls')),
 
 )
+
+# expose static files and uploded media if DEBUG is active
+if settings.DEBUG:
+    urlpatterns += patterns(
+        '',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+            {
+                'document_root': settings.MEDIA_ROOT,
+                'show_indexes': True
+            }),
+        url(r'', include('django.contrib.staticfiles.urls')),
+    )
