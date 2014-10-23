8d7cde3fb6fc3aff028748382291f873349aa352
Dražen Odobašić <dodobas@candela-it.com>
2014-10-23 14:47:51 +0200

    Remove "site" application

--- django_project/core/settings/base.py
+++ django_project/core/settings/base.py
@@ -78,7 +78,6 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
